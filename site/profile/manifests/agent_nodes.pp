f38663c704d43a209df5c445ddfbd1f2c8697db3
kdgs <kelsong@gmail.com>
2018-02-19 11:46:42 -0300

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,5 @@ class profile::agent_nodes{
   include dockeragent
   dockeragent::node { 'web.puppet.local':}
   dockeragent::node { 'db.puppet.local':}
+  dockeragent::node { 'mc.puppet.local':}
 }

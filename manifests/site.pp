b9ff512f6cce6a824f0822b2e51cebe62877f785
kdgs <kelsong@gmail.com>
2018-02-20 12:44:43 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,40 @@
 node default {
+  notify {"Default Node Reached":
+    message   => "\n\n\nConfiguração Padrão de Node\n\n\n"
   }
+}
 node 'master.puppet.local' {
   include role::master_server
   file { '/root/README':
     ensure  => file,
     content => "Bem vindo ao ${fqdn}\n",
   }
+  notify {"Master Node Reached":
+    message   => "\n\n\nConfiguração do Node MASTER\n\n\n"
+  }
 }
 
 node 'mc.puppet.local' {
   include role::minecraft_server
+  notify {"Minecraft Server Node Reached":
+    message   => "\n\n\nTeste de configuração do modulo Minecraft\n\n\n"
+  }
 }
 
 node 'web.puppet.local' {
   include role::app_server
   include java::install
+  
+  notify {"Web Node Reached":
+    message   => "\n\n\nConfiguração do Node Web!!!!\nTestando instalação de pacotes Java e criação de novo módulo.\n\n\n"
   }
+  
+}
 node /^db/ {
   include role::db_server
+  
+  notify {"DB Node Reached":
+    message   => "\n\n\nConfiguração do Node de Banco de Dados\n\n\n"
+  }
+
 }

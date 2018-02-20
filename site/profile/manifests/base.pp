410c3902f2fd25fb29f6978f76cad839cdea2d2c
kdgs <kelsong@gmail.com>
2018-02-20 10:54:51 -0300

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,12 @@
 class profile::base {
   user {'admin':
     ensure => present,
-    }
+  }
+  file { '/etc/ssh/sshd_config':
+    ensure => present,
+   }->
+  file_line { 'Append a line to /etc/ssh/sshd_config':
+    path => '/etc/ssh/sshd_config',  
+    line => 'Banner /etc/issue.net',
+  }
 }

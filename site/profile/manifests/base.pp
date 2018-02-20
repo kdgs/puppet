7eb903e51f7519c2d40f7c5dc3592813ee6eabef
kdgs <kelsong@gmail.com>
2018-02-20 11:49:53 -0300

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,36 +1,37 @@
 class profile::base {
   user {'admin':
-    ensure  => present,
+    ensure      => present,
   }
   file { '/etc/ssh/sshd_config':
-    ensure  => present,
+    ensure      => present,
    }->
   file_line { 'Append a line to /etc/ssh/sshd_config':
-    notify  => Service['sshd'],
-    path    => '/etc/ssh/sshd_config',  
-    line    => 'Banner /etc/issue.net',
+    notify      => Service['sshd'],
+    path        => '/etc/ssh/sshd_config',  
+    line        => 'Banner /etc/issue.net',
   }
   file {'/etc/issue.net':
-    ensure  => file,
-    source  => 'puppet:///modules/config_geral/issue.net',
+    ensure      => file,
+    source      => 'puppet:///modules/config_geral/issue.net',
   }
   file {'/etc/motd':
-    ensure  => file,
-    source  => 'puppet:///modules/config_geral/motd',
+    ensure      => file,
+    source      => 'puppet:///modules/config_geral/motd',
   }
   service { sshd:
-    ensure  => running
+    ensure      => running
   }
   package {['tree','wget','git','unzip','ntp']: 
-    ensure  => 'installed',
+    ensure      => 'installed',
   }
   
   user { 'teste':
-    ensure  => absent,
+    ensure      => absent,
+    managehome  => true,
   }
   service { 'ntpd':
-    require  => Package['ntp'],
-    ensure  => running,
-    enable  => true,
+    require     => Package['ntp'],
+    ensure      => running,
+    enable      => true,
   }
 }

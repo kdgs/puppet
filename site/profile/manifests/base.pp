ad2ee0e9231246f6ac4c6ef5d4c48caaeca64dfa
kdgs <kelsong@gmail.com>
2018-02-20 11:35:19 -0300

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,14 +1,14 @@
 class profile::base {
   user {'admin':
-    ensure => present,
+    ensure  => present,
   }
   file { '/etc/ssh/sshd_config':
-    ensure => present,
+    ensure  => present,
    }->
   file_line { 'Append a line to /etc/ssh/sshd_config':
     notify  => Service['sshd'],
-    path => '/etc/ssh/sshd_config',  
-    line => 'Banner /etc/issue.net',
+    path    => '/etc/ssh/sshd_config',  
+    line    => 'Banner /etc/issue.net',
   }
   file {'/etc/issue.net':
     ensure  => file,
@@ -19,6 +19,16 @@ class profile::base {
     source  => 'puppet:///modules/config_geral/motd',
   }
   service { sshd:
-    ensure => running
+    ensure  => running
+  }
+  package {['tree','wget','git','unzip','ntp']:
+    ensure  => installed,
+  }
+  user { 'teste':
+    ensure  => absent,
+  }
+  service { 'ntp':
+    ensure  => running,
+    enable  => true,
   }
 }
